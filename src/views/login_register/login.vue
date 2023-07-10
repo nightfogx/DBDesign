af89d3b4975117703b9bb437c9d6d555d8ae806c
陈瑾旭 <108864288+RidenShogunei@users.noreply.github.com>
2023-07-10 08:40:41 +0800

    7.10更新
    7.10最新，更新了axios脚本和mockjs部分

--- src/views/login_register/login.vue
+++ src/views/login_register/login.vue
@@ -1,8 +1,8 @@
 <template>
   <ElForm class="login-form" ref="loginForm" :model="loginForm" :rules="rules">
     <h1 class="login-title">登录</h1>
-    <ElFormItem prop="username">
-      <ElInput placeholder="请输入id" :prefix-icon="icons.UserFilled"  v-model="loginForm.username" size="large"></ElInput>
+    <ElFormItem prop="userid">
+      <ElInput placeholder="请输入id" :prefix-icon="icons.UserFilled" v-model="loginForm.userid" size="large"></ElInput>
     </ElFormItem>
     <ElFormItem prop="password">
       <el-input placeholder="请输入密码" :prefix-icon="icons.Lock" v-model="loginForm.password" size="large"></el-input>
@@ -11,12 +11,13 @@
       <el-input placeholder="请输入验证码" :prefix-icon="icons.QuestionFilled" v-model="loginForm.code" size="large"></el-input>
     </ElFormItem>
     <ElFormItem>
-      <ElButton type="primary" class="login-btn" size="large" @click="signUp" >登录</ElButton>
+      <ElButton type="primary" class="login-btn" size="large" @click="submitForm">登录</ElButton>
     </ElFormItem>
   </ElForm>
 </template>
 
 <script>
+import { login } from '@/api/login.js'
 import * as icons from '@element-plus/icons-vue';
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
@@ -27,10 +28,10 @@ export default {
       // 表单信息
       loginForm: {
         // 账户数据
-        username: "",
+        userid: "",
         // 密码数据
         password: "",
-        
+
         // 验证码数据
         code: "",
         // 记住密码
@@ -41,8 +42,8 @@ export default {
       // 表单验证
       rules: {
         // 设置账户效验规则
-        username: [
-          { required: true, message: "请输入账户", trigger: "blur" },
+        userid: [
+          { required: true, message: "请输入账户id", trigger: "blur" },
           { min: 3, max: 10, message: "长度在 3 到 10 个字符的账户", trigger: "blur" },
         ],
         // 设置密码效验规则
@@ -58,16 +59,46 @@ export default {
       },
       // 绑定验证码图片
       codeImg: null,
-      // 后端提供的URL
-      apiUrl: "/api/login",
     };
   },
   methods: {
-    signUp()
-    {
+    resetForm() {//重置表单
+
+      // 账户数据
+      this.loginForm.userid = "",
+        // 密码数据
+        this.loginForm.password = "",
+        // 验证码数据
+        this.loginForm.code = "",
+        // 记住密码
+        this.loginForm.remember = false,
+        // 验证码的key，因为前后端分离，这里验证码不能由后台存入session，所以交给vue状态管理
+        this.loginForm.codeToken = ""
+    },
+    signUp() {
       this.$router.push('/mainpage')
-    }
+    },
     // 提交表单
+    submitForm() {
+      console.log("点击了登录键")
+      //请求地址,this和vm指的是全局
+      let params = {
+        user: this.loginForm.userid,
+        pass: this.loginForm.password,
+      }
+      console.log(params)
+      login(params).then((res) => {
+        console.log(res.data)
+        if (res.data === false) {
+          console.log("登录失败")
+          this.resetForm();
+        }
+        else {
+          console.log("登录成功")
+          this.$router.push('/mainpage')
+        }
+      })
+    },
   },
 
 };
@@ -92,15 +123,15 @@ export default {
 }
 
 .login-title {
-  
+
   /*设置字体*/
   font-size: 60px;
   /*设置字体大小*/
   font-weight: bolder;
   /*设置字体粗细*/
- 
+
   /*文字描边*/
-color: #5c9ef5;
+  color: #5c9ef5;
   /*设置文字的填充颜色*/
 
   font-size: 50px;
@@ -111,4 +142,5 @@ color: #5c9ef5;
 .login-btn {
   width: 100%;
   font-size: 18px;
-}</style>
+}
+</style>
